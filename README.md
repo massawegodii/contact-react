88b6fd239c5ef74f6c73d332bd287cefcfca2233
davidmboya08 <davidmboya08@gmail.com>
2024-07-28 16:54:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+React spring boot project
---
+++ src/main/java/com/massawe/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.massawe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization"));
+        corsConfiguration.setExposedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+}
